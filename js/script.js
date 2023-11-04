a454fbbb05f710da207e2ec074ce8103af056353
giovannimegliola <giovanni.megliola@hotmail.it>
2023-11-04 20:07:40 +0100

    milestone 3 completed

--- js/script.js
+++ js/script.js
@@ -11,19 +11,65 @@ createApp({
       activeContactIndex: 0,
       searchText: '',
       message: '',
+      answer: 'ok'
     }
   },
   methods:{
     selectContact(id){
-      this.activeContactIndex = this.contacts.findIndex((contact)=> contact.id === id);
+      const index = this.contacts.findIndex ((contact)=> contact.id === id);
+      if (index !== -1){
+        this.activeContactIndex = index;
+      }
+    },
+    getLastMessage(id){
+      const contact = this.contacts.find((contact)=> contact.id === id);
+      const len = contact.messages.length;
+      if (len > 0){
+        return contact.messages[len - 1].message;
+      } else {
+        return 'non ci sono messaggi';
+      }    
+    },
+    getLastAccess(id){
+      const contact = this.contacts.find((contact)=> contact.id === id);
+      const len = contact.messages.length;
+      if (len > 0){
+        return contact.messages[len - 1].date;
+      } else {
+        return 'sconosciuto';
+      }
+    },
+    newMessage(){
+      const newmsg = {
+        date: new Date(),
+        message: this.message,
+        status: 'sent'
+      }
+      this.activeContact.messages.push(newmsg);
+      this.message = '';
+      setTimeout(()=>{
+        const newmsg = {
+          date: new Date(),
+          message: this.answer,
+          status: 'received'
+        }
+        this.activeContact.messages.push(newmsg);
 
+
+      }, 1000)
     }
   },
   computed:{
-
+    activeContact(){
+      return this.contacts[this.activeContactIndex];
+    },
+    lastDate(){
+      const len = this.activeContact.messages.length;
+      if (len > 0){
+        return this.activeContact.messages[len - 1].date;
+      } else {
+        return 'sconosciuto';
+      }  
+    }
   }
-
-}
-
-
-).mount('#app');
+}).mount('#app');
